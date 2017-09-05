2cd881b7cdf44b5f45ee2341647eebcb2239510d
Vitaliy Grechachin <vitaley.grechachin@gmail.com>
2017-09-05 14:02:16 +0500

    done

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-port = int(os.getenv("VCAP_APP_PORT"))
 
 from app import views, models, forms
